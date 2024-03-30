80cff2567d23d54596d7c5e2bea36036b365a8b7
Rodrigo <rodrigo.rm123.rm@gmail.com>
2024-03-30 11:13:33 -0300

    feat: add react-hot-toast and setting at app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
+import { Toaster } from 'react-hot-toast';
 import { Router } from './routes';
 
 export function App() {
   return (
-    <Router/>
+    <>
+      <Router/>
+      <Toaster
+        position='bottom-right'
+      />
+    </>
   );
 }
 
